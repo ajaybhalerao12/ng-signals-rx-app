fdb91feac6853a717a9b8420a1964d570387fa8e
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-11-07 20:18:39 +0530

    feat: Refactor cart item component to use signals for reactive state management

--- src/app/cart/cart-item/cart-item.component.ts
+++ src/app/cart/cart-item/cart-item.component.ts
@@ -1,30 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
 import { CartItem } from '../cart';
 import { FormsModule } from '@angular/forms';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cart-item.component.html',
-  styleUrl: './cart-item.component.css'
+  styleUrl: './cart-item.component.css',
 })
 export class CartItemComponent {
-  @Input({ required: true }) cartItem!: CartItem;
+  @Input({ required: true }) set cartItem(ci:CartItem){
+    this.item.set(ci);
+  }
+
+  item =signal<CartItem>(undefined!);
+
+  private cartSvc = inject(CartService);
 
   // Quantity available (hard-coded to 8)
   // Mapped to an array from 1-8
-  qtyArr = [...Array(8).keys()].map(x => x + 1);
+  qtyArr = [...Array(8).keys()].map((x) => x + 1);
 
   // Calculate the extended price
-  exPrice = this.cartItem?.quantity * this.cartItem?.product.price;
+  // exPrice = this.item()?.quantity * this.item()?.product.price;
+  exPrice = computed(() => this.item().quantity * this.item().product.price);
 
   onQuantitySelected(quantity: number): void {
-
+    this.cartSvc.updateQuantity(this.item(), Number(quantity));
   }
 
   removeFromCart(): void {
-
+    this.cartSvc.removeFromCart(this.item());
   }
 }
